2dda0c6007a6dcf858198a04c38c214555e40c3c
Iconer <yoyofa2@hotmail.com>
2023-09-11 15:41:05 +0900

    Tiled scripts added

--- res/Everything.tsx
+++ res/Everything.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Everything" tilewidth="1" tileheight="1" tilecount="0" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="Everything" tilewidth="16" tileheight="16" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
  <transformations hflip="0" vflip="0" rotate="0" preferuntransformed="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="../GaugeGame/sprites/sDryDirt1/025f57e6-cdbb-4e31-af1f-b42546994b46.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="../GaugeGame/sprites/sTempleBrick1/f7202e98-9f37-4381-9633-9a10e1cbfc46.png"/>
+ </tile>
 </tileset>
