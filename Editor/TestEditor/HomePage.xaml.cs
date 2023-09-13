1d3bf559dd8f02981e8d7918a9ff9a3926c46b8f
Iconer <yoyofa2@hotmail.com>
2023-09-13 14:42:58 +0900

    Homepage layout modified

--- Editor/TestEditor/HomePage.xaml.cs
+++ Editor/TestEditor/HomePage.xaml.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,6 +11,10 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,9 +26,14 @@ namespace TestEditor
     /// </summary>
     public sealed partial class HomePage : Page
     {
-        public HomePage()
+        private static readonly Color Transparent = Color.FromArgb(0, 0, 0, 0);
+
+		public HomePage()
         {
             this.InitializeComponent();
+
+			//homeContent.Background = new SolidColorBrush(Transparent);
+			
 		}
     }
 }
