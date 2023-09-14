405f636619e00c392ceceec3a96ab6584f9e5dff
Iconer <yoyofa2@hotmail.com>
2023-09-15 01:19:47 +0900

    Cleanup `using`s

--- Editor/TestEditor/FilePickHelper.cs
+++ Editor/TestEditor/FilePickHelper.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Windows.Foundation;
+﻿using Windows.Foundation;
 using Windows.Storage;
 using Windows.Storage.Pickers;
-using Windows.Storage.Streams;
 
 namespace TestEditor
 {
