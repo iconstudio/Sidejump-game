050ead3d4f86a7c9e8c818502519166a91659eb8
Iconer <yoyofa2@hotmail.com>
2023-09-14 21:37:55 +0900

    new `Tileset` class

--- Editor/TestEditor/Map.cs
+++ Editor/TestEditor/Map.cs
@@ -25,12 +25,4 @@ namespace TestEditor
 			return JsonSerializer.Serialize(this);
 		}
 	}
-
-	[Serializable]
-	internal class Tileset
-	{
-		public Dictionary<int, string> tileData;
-		public uint tileWidth;
-		public uint tileHeight;
-	}
 }
---
+++ Editor/TestEditor/Tileset.cs
@@ -0,0 +1,11 @@
+﻿namespace TestEditor
+{
+
+	[Serializable]
+	internal class Tileset
+	{
+		public Dictionary<int, string> tileData;
+		public uint tileWidth;
+		public uint tileHeight;
+	}
+}
