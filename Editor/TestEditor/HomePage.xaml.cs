a529ba1bb527197aacaa70e408176172a87b6d5a
Iconer <yoyofa2@hotmail.com>
2023-09-14 22:10:09 +0900

    Cleanup comments

--- Editor/TestEditor/HomePage.xaml.cs
+++ Editor/TestEditor/HomePage.xaml.cs
@@ -5,14 +5,8 @@ using Microsoft.UI.Xaml.Input;
 using Windows.ApplicationModel;
 using Windows.UI;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace TestEditor
 {
-	/// <summary>
-	/// An empty page that can be used on its own or navigated to within a Frame.
-	/// </summary>
 	public sealed partial class HomePage : Page
 	{
 		private static readonly Color Transparent = Color.FromArgb(0, 0, 0, 0);
