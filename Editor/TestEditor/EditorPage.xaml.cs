bda5f3181d2d1acfd11953094c030adc38a6399f
Iconer <yoyofa2@hotmail.com>
2023-09-15 01:36:15 +0900

    `EditorPage` would not save & load maps

--- Editor/TestEditor/EditorPage.xaml.cs
+++ Editor/TestEditor/EditorPage.xaml.cs
@@ -1,7 +1,3 @@
-using System.Diagnostics;
-
-using Windows.Storage;
-
 namespace TestEditor
 {
 	public sealed partial class EditorPage : Page
@@ -11,40 +7,5 @@ namespace TestEditor
 			InitializeComponent();
 		}
 
-		public async
-			void
-			LoadMap(StorageFile file)
-		{
-			if (file is null)
-			{
-				throw new ArgumentNullException(nameof(file));
-			}
-
-			using var fstream = await StorageHelper.ReadFile(file);
-
-			if (fstream is null || !fstream.CanRead)
-			{
-				Debug.Print("Inappropriate file stream");
-				return;
-			}
-
-			var length = fstream.Length;
-			if (0 == length)
-			{
-				Debug.Print("Zero sized file");
-				return;
-			}
-
-			fstream.Seek(0, SeekOrigin.Begin);
-
-			byte[] mbuffer = new byte[length];
-			var work_sz = await fstream.ReadAsync(mbuffer);
-
-			if (0 == work_sz)
-			{
-				Debug.Print("Read zero size");
-				return;
-			}
-		}
 	}
 }
