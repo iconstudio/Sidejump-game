85c88e2cdc33630f72a30ff55a78851772ad313f
Iconer <yoyofa2@hotmail.com>
2023-09-14 20:23:16 +0900

    `NavigationHelper` is added

---
+++ Editor/TestEditor/NavigationHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml.Media.Animation;
+
+namespace TestEditor
+{
+	internal static class NavigationHelper
+	{
+		public static MainWindow CurrentWindow => MainWindow.GetInstance();
+
+		public static
+			void
+			Goto(Type target, object parameter = null, NavigationTransitionInfo transition = null, Type current = null)
+		{
+			if (target != current)
+			{
+				if (transition is null)
+				{
+					CurrentWindow.GetContent().Navigate(target, parameter);
+				}
+				else
+				{
+					CurrentWindow.GetContent().Navigate(target, parameter, transition);
+				}
+			}
+		}
+	}
+}
