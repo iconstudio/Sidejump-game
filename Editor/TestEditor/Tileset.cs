e33e8528539f4894595191477b579d0ae7ed3fd6
Iconer <yoyofa2@hotmail.com>
2023-09-15 01:03:21 +0900

    `Tileset` can be refered by tile id

--- Editor/TestEditor/Tileset.cs
+++ Editor/TestEditor/Tileset.cs
@@ -5,5 +5,11 @@
 		public FlatMap<int, string> tileData;
 		public uint tileWidth;
 		public uint tileHeight;
+
+		public string this[int tile_id]
+		{
+			get { return tileData[tile_id]; }
+			set { tileData[tile_id] = value; }
+		}
 	}
 }
