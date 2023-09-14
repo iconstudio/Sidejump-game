47e7ad7b03b55e99ba63a97939cc370efc689a7e
Iconer <yoyofa2@hotmail.com>
2023-09-14 19:40:16 +0900

    File pickers are modified

--- Editor/TestEditor/FilePickHelper.cs
+++ Editor/TestEditor/FilePickHelper.cs
@@ -4,61 +4,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Windows.Foundation;
 using Windows.Storage;
 using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
 
 namespace TestEditor
 {
 	internal static class FilePickHelper
 	{
-		public static async Task<StorageFile> OpenSavePicker(Window window)
-		{
-			FileSavePicker picker = new();
-			picker.FileTypeChoices.Add("Map File", new List<string>() { ".gmap" });
-			picker.FileTypeChoices.Add("Archieved Map File", new List<string>() { ".zip" });
-
-			var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
-			WinRT.Interop.InitializeWithWindow.Initialize(picker, hWnd);
-
-			return await picker.PickSaveFileAsync();
-		}
-		public static async Task<StorageFile> OpenLoadPicker(Window window)
+		public static IAsyncOperation<StorageFile> OpenLoadPicker(Window window)
 		{
 			FileOpenPicker picker = new()
 			{
 				ViewMode = PickerViewMode.Thumbnail
 			};
+			picker.FileTypeFilter.Clear();
 			picker.FileTypeFilter.Add(".gmap");
 
-			var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
-			WinRT.Interop.InitializeWithWindow.Initialize(picker, hWnd);
+			var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+			WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
 
-			return await picker.PickSingleFileAsync();
+			return picker.PickSingleFileAsync();
 		}
-		public static async Task<FileStream> TryOpenFile(Task<StorageFile> ftask)
+		public static IAsyncOperation<StorageFile> OpenSavePicker(Window window)
 		{
-			if (ftask is not null)
+			FileSavePicker picker = new();
+			picker.FileTypeChoices.Clear();
+			picker.FileTypeChoices.Add("Map File", new List<string>() { ".gmap" });
+			//picker.FileTypeChoices.Add("Archieved Map File", new List<string>() { ".zip" });
+
+			var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+			WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
+
+			return picker.PickSaveFileAsync();
+		}
+		public static async
+			Task<Stream>
+			TryOpenReadOnlyFile(StorageFile file)
+		{
+			if (file is not null)
 			{
-				await ftask;
-
-				if (ftask.IsCompletedSuccessfully)
-				{
-					var file = ftask.Result;
-					if (file is null)
-					{
-						goto last;
-					}
-
-					if (!file.IsAvailable)
-					{
-						goto last;
-					}
-
-					return (FileStream) file.OpenReadAsync();
-				}
+				return await file.OpenStreamForReadAsync();
 			}
 
-		last:
+			return null;
+		}
+		public static async
+			Task<Stream>
+			TryOpenReadOnlyFile(IAsyncOperation<StorageFile> ftask)
+		{
+			var file = await ftask;
+			if (file is not null)
+			{
+				return await file.OpenStreamForReadAsync();
+			}
+
+			return null;
+		}
+		public static async
+			Task<Stream>
+			TryOpenWriteFile(StorageFile file)
+		{
+			if (file is not null)
+			{
+				return await file.OpenStreamForWriteAsync();
+			}
+
+			return null;
+		}
+		public static async
+			Task<Stream>
+			TryOpenWriteFile(IAsyncOperation<StorageFile> ftask)
+		{
+			var file = await ftask;
+			if (file is not null)
+			{
+				return await file.OpenStreamForWriteAsync();
+			}
+
 			return null;
 		}
 	}
