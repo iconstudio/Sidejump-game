c716e26bd8cf82eacf053eb68335cfee7cf4e31e
Iconer <yoyofa2@hotmail.com>
2023-09-14 21:49:11 +0900

    `FlatMap` is modified

--- Editor/TestEditor/FlatMap.cs
+++ Editor/TestEditor/FlatMap.cs
@@ -1,25 +1,21 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace TestEditor
 {
 	internal class FlatMap<T, V>
-		: IDictionary<T, V> where T : notnull
+		: IDictionary<T, V>, IReadOnlyDictionary<T, V>, ISerializable, IDeserializationCallback
+		where T : notnull
 	{
-		public V this[T key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-		public ICollection<T> Keys => throw new NotImplementedException();
-
-		public ICollection<V> Values => throw new NotImplementedException();
+		public List<KeyValuePair<T, V>> myData;
 
 		public int Count => throw new NotImplementedException();
-
 		public bool IsReadOnly => throw new NotImplementedException();
+		public ICollection<T> Keys => throw new NotImplementedException();
+		public ICollection<V> Values => throw new NotImplementedException();
+		IEnumerable<T> IReadOnlyDictionary<T, V>.Keys => throw new NotImplementedException();
+		IEnumerable<V> IReadOnlyDictionary<T, V>.Values => throw new NotImplementedException();
 
 		public void Add(T key, V value)
 		{
@@ -37,7 +33,16 @@ namespace TestEditor
 		{
 			throw new NotImplementedException();
 		}
+		public V this[T key]
+		{
+			get => throw new NotImplementedException();
+			set => throw new NotImplementedException();
+		}
 		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+		public void GetObjectData(SerializationInfo info, StreamingContext context)
 		{
 			throw new NotImplementedException();
 		}
@@ -62,6 +67,10 @@ namespace TestEditor
 			throw new NotImplementedException();
 		}
 		public void CopyTo(KeyValuePair<T, V>[] array, int arrayIndex)
+		{
+			throw new NotImplementedException();
+		}
+		public void OnDeserialization(object sender)
 		{
 			throw new NotImplementedException();
 		}
