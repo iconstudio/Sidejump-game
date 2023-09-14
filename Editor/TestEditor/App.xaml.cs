979248e81cb9bc56c765a0a997dcb027266cd08b
Iconer <yoyofa2@hotmail.com>
2023-09-14 20:02:18 +0900

    Cleanup comments

--- Editor/TestEditor/App.xaml.cs
+++ Editor/TestEditor/App.xaml.cs
@@ -8,9 +8,6 @@ using Windows.Win32.UI.WindowsAndMessaging;
 
 namespace TestEditor
 {
-	/// <summary>
-	/// Provides application-specific behavior to supplement the default Application class.
-	/// </summary>
 	public partial class App : Application
 	{
 		private Window m_window;
@@ -19,19 +16,11 @@ namespace TestEditor
 		private int minWidth = 600;
 		private int minHeight = 400;
 
-		/// <summary>
-		/// Initializes the singleton application object.  This is the first line of authored code
-		/// executed, and as such is the logical equivalent of main() or WinMain().
-		/// </summary>
 		public App()
 		{
 			InitializeComponent();
 		}
 
-		/// <summary>
-		/// Invoked when the application is launched.
-		/// </summary>
-		/// <param name="args">Details about the launch request and process.</param>
 		protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
 		{
 			m_window = new MainWindow();
