2528bd1639fd78c16e9f6ec8db72944468688658
Iconer <yoyofa2@hotmail.com>
2023-09-16 07:13:00 +0900

    `EditorTransitionInfo` has static prefabs

--- Editor/TestEditor/EditorTransitionInfo.cs
+++ Editor/TestEditor/EditorTransitionInfo.cs
@@ -8,6 +8,18 @@
 	[Serializable]
 	internal readonly struct EditorTransitionInfo
 	{
+		public static readonly EditorTransitionInfo homeTransition;
+		public static readonly EditorTransitionInfo saveTransition;
+		public static readonly EditorTransitionInfo loadTransition;
+		public static readonly EditorTransitionInfo exitTransition;
+
+		static EditorTransitionInfo()
+		{
+			homeTransition = new EditorTransitionInfo(EditorTransitionCategory.Home);
+			saveTransition = new EditorTransitionInfo(EditorTransitionCategory.Create);
+			loadTransition = new EditorTransitionInfo(EditorTransitionCategory.Load);
+			exitTransition = new EditorTransitionInfo(EditorTransitionCategory.Exit);
+		}
 		public EditorTransitionInfo()
 		{
 			transitionCategory = EditorTransitionCategory.Home;
