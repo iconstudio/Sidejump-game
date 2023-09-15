9e52c8472e329c7ec02ac6ad9f6b257e03a852d2
Iconer <yoyofa2@hotmail.com>
2023-09-16 00:17:21 +0900

    `EditorPage` would receive `EditorTransitionInfo` at method `OnNavigatedTo`

--- Editor/TestEditor/EditorPage.xaml.cs
+++ Editor/TestEditor/EditorPage.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.UI.Xaml.Navigation;
+
 namespace TestEditor
 {
 	public sealed partial class EditorPage : Page
@@ -5,6 +7,21 @@ namespace TestEditor
 		public EditorPage()
 		{
 			InitializeComponent();
+		}
+
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			base.OnNavigatedTo(e);
+
+			if (e.Parameter is EditorTransitionInfo info)
+			{
+				OnTransition(info.transitionCategory);
+			}
+		}
+		private void OnTransition(EditorTransitionCategory cat)
+		{
+			//var testmap = new Map();
+			//using (MapHelper.SaveMap(testmap, mapfile))
 		}
 	}
 }
