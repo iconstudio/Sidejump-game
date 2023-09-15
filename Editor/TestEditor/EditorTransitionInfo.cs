5e2b1ac040af0085ba15d2089d0b40395ff5fef4
Iconer <yoyofa2@hotmail.com>
2023-09-16 00:14:15 +0900

    new struct `EditorTransitionInfo` for `EditorPage`

---
+++ Editor/TestEditor/EditorTransitionInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace TestEditor
+{
+	internal enum EditorTransitionCategory
+	{
+		Home, Create, Load, Exit
+	}
+
+	[Serializable]
+	internal readonly struct EditorTransitionInfo
+	{
+		public EditorTransitionInfo()
+		{
+			transitionCategory = EditorTransitionCategory.Home;
+		}
+		public EditorTransitionInfo(EditorTransitionCategory category)
+		{
+			transitionCategory = category;
+		}
+
+		public readonly EditorTransitionCategory transitionCategory;
+	}
+}
