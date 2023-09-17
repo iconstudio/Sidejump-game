3fc2c4ccd197b1a45cb8b7070fdaf378f8632ad2
Iconer <yoyofa2@hotmail.com>
2023-09-18 03:56:30 +0900

    `DispatcherQueueExtension` provides many alternative methods

--- Editor/TestEditor/DispatcherQueueExtension.cs
+++ Editor/TestEditor/DispatcherQueueExtension.cs
@@ -7,10 +7,11 @@ using Windows.Foundation;
 namespace TestEditor
 {
 	using DispatcherQueueTimerTicket = TypedEventHandler<DispatcherQueueTimer, object>;
+	using DispatcherQueueTimerAlterTicket = DispatcherQueueHandler;
 
-	internal enum DispatcherContinuationPolicy
+	internal enum DispatcherFailurePolicy
 	{
-
+		DoNotContinue, ExcuteAnyway, ExecuteNow
 	}
 
 	internal static class DispatcherQueueExtension
@@ -26,17 +27,29 @@ namespace TestEditor
 		}
 		public static
 			DispatcherQueueTimer
-			CreateStaticTask(this DispatcherQueue queue, [DisallowNull] DispatcherQueueTimerTicket ticket)
+			CreateStaticTask(this DispatcherQueue queue
+				, [DisallowNull] DispatcherQueueTimerTicket ticket)
 		{
-			var task_timer = queue.CreateTimer();
-			task_timer?.SetLoop(false);
+			var task_timer = CreateStaticTask(queue);
 			task_timer?.AddEventHandler(ticket);
 
 			return task_timer;
 		}
 		public static
 			DispatcherQueueTimer
-			CreateTask(this DispatcherQueue queue, in TimeSpan duration, bool loop = false)
+			CreateStaticTask(this DispatcherQueue queue
+				, [DisallowNull] DispatcherQueueTimerAlterTicket ticket)
+		{
+			var task_timer = CreateStaticTask(queue);
+			task_timer?.AddEventHandler((s, e) => { ticket(); });
+
+			return task_timer;
+		}
+		public static
+			DispatcherQueueTimer
+			CreateTask(this DispatcherQueue queue
+				, in TimeSpan duration
+				, bool loop = false)
 		{
 			var task_timer = queue.CreateTimer();
 			task_timer?.SetLoop(loop);
@@ -46,12 +59,66 @@ namespace TestEditor
 		}
 		public static
 			DispatcherQueueTimer
-			CreateTask(this DispatcherQueue queue, [DisallowNull] DispatcherQueueTimerTicket ticket, in TimeSpan duration, bool loop = false)
+			CreateTask(this DispatcherQueue queue
+				, [DisallowNull] DispatcherQueueTimerTicket ticket
+				, in TimeSpan duration
+				, bool loop = false)
 		{
-			var task_timer = queue.CreateTimer();
-			task_timer?.SetLoop(loop);
-			task_timer?.SetInterval(duration);
+			var task_timer = CreateTask(queue, duration, loop);
 			task_timer?.AddEventHandler(ticket);
+
+			return task_timer;
+		}
+		public static
+			DispatcherQueueTimer
+			CreateTask(this DispatcherQueue queue
+				, [DisallowNull] DispatcherQueueTimerAlterTicket ticket
+				, in TimeSpan duration
+				, bool loop = false)
+		{
+			var task_timer = CreateTask(queue, duration, loop);
+			task_timer?.AddEventHandler((s, e) => { ticket(); });
+
+			return task_timer;
+		}
+		public static
+			DispatcherQueueTimer
+			StartTask(this DispatcherQueue queue
+				, DispatcherFailurePolicy policy
+				, [DisallowNull] DispatcherQueueTimerAlterTicket ticket
+				, in TimeSpan duration
+				, bool loop = false)
+		{
+			var task_timer = CreateTask(queue, duration, loop);
+			if (task_timer is null)
+			{
+				switch (policy)
+				{
+					case DispatcherFailurePolicy.DoNotContinue:
+					{ }
+					break;
+
+					case DispatcherFailurePolicy.ExcuteAnyway:
+					{
+						queue.TryEnqueue(DispatcherQueuePriority.Normal, ticket);
+					}
+					break;
+
+					case DispatcherFailurePolicy.ExecuteNow:
+					{
+						ticket();
+					}
+					break;
+
+					default:
+					break;
+				}
+			}
+			else
+			{
+				task_timer.AddEventHandler((s, e) => { ticket(); });
+				task_timer.Start();
+			}
 
 			return task_timer;
 		}
