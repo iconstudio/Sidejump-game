b3f592e8229a6322e4311062c645f373463a6063
Iconer <yoyofa2@hotmail.com>
2023-09-19 15:29:16 +0900

    `Style` and `Option` classes for `WindowView`

--- Editor/TestEditor/WinUI/WindowView.cs
+++ Editor/TestEditor/WinUI/WindowView.cs
@@ -8,8 +8,7 @@ using Windows.Foundation;
 using Windows.UI.Core;
 using Windows.Win32;
 using Windows.Win32.Foundation;
-using Windows.Win32.UI;
-using Windows.Win32.UI.Shell;
+using Windows.Win32.UI.WindowsAndMessaging;
 
 namespace TestEditor.WinUI
 {
@@ -19,7 +18,89 @@ namespace TestEditor.WinUI
 
 	internal struct WindowView : IEquatable<WindowView>
 	{
+		internal class Style : IEquatable<Style>, IComparable<Style>
+		{
+			internal nint value;
+
+			public Style(nint style = 0)
+			{
+				value = style;
+			}
+
+			[Pure]
+			public override bool Equals(object other)
+			{
+				if (other is null)
+					return false;
+
+				return other.Equals(this);
+			}
+			[Pure]
+			public bool Equals(Style other)
+			{
+				return value == other.value;
+			}
+			[Pure]
+			public int CompareTo(Style other)
+			{
+				return value.CompareTo(other.value);
+			}
+			[Pure]
+			public override int GetHashCode()
+			{
+				return value.GetHashCode();
+			}
+
+			public static explicit operator nint(Style style)
+			{
+				return style.value;
+			}
+		}
+		internal class Option : IEquatable<Option>, IComparable<Option>
+		{
+			internal nint value;
+
+			public Option(WINDOW_EX_STYLE options)
+				: this((nint) options)
+			{ }
+			public Option(nint options = 0)
+			{
+				value = options;
+			}
+
+			[Pure]
+			public override bool Equals(object other)
+			{
+				if (other is null)
+					return false;
+
+				return other.Equals(this);
+			}
+			[Pure]
+			public bool Equals(Option other)
+			{
+				return value == other.value;
+			}
+			[Pure]
+			public int CompareTo(Option other)
+			{
+				return value.CompareTo(other.value);
+			}
+			[Pure]
+			public override int GetHashCode()
+			{
+				return value.GetHashCode();
+			}
+
+			public static explicit operator nint(Option option)
+			{
+				return option.value;
+			}
+		}
+
 		private WindowSubRoutine mySubRoutine;
+		private Style myStyles;
+		private Option myOptions;
 
 		public readonly Window Implement { get; init; }
 		public readonly HWND NativeHandle { get; init; }
@@ -60,7 +141,25 @@ namespace TestEditor.WinUI
 			set
 			{
 				mySubRoutine = new(value);
-                PInvoke.SetWindowSubclass(NativeHandle, mySubRoutine, 0, 0);
+				PInvoke.SetWindowSubclass(NativeHandle, mySubRoutine, 0, 0);
+			}
+		}
+		public Style Styles
+		{
+			readonly get => myStyles;
+			set
+			{
+				myStyles = value;
+				PInvoke.SetWindowLongPtr(NativeHandle, WINDOW_LONG_PTR_INDEX.GWL_STYLE, (nint) value);
+			}
+		}
+		public Option Options
+		{
+			readonly get => myOptions;
+			set
+			{
+				myOptions = value;
+				PInvoke.SetWindowLongPtr(NativeHandle, WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE, (nint) value);
 			}
 		}
 
@@ -71,11 +170,34 @@ namespace TestEditor.WinUI
 			IntPtr hwnd = WinRT.Interop.WindowNative.GetWindowHandle(Implement);
 			NativeHandle = new(hwnd);
 			mySubRoutine = null;
+
+			myStyles = new(PInvoke.GetWindowLongPtr(NativeHandle, WINDOW_LONG_PTR_INDEX.GWL_STYLE));
+			myOptions = new(PInvoke.GetWindowLongPtr(NativeHandle, WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE));
 		}
 
-		public void Activate() => Implement?.Activate();
-		public void Close() => Implement?.Activate();
-		public void SetTitleBar(UIElement titleBar) => Implement?.SetTitleBar(titleBar);
+		public readonly void Activate() => Implement?.Activate();
+		public readonly void Close() => Implement?.Activate();
+		public readonly void SetTitleBar(UIElement titleBar) => Implement?.SetTitleBar(titleBar);
+		public readonly void AddStyle(nint style)
+		{
+			myStyles.value |= style;
+			PInvoke.SetWindowLongPtr(NativeHandle, WINDOW_LONG_PTR_INDEX.GWL_STYLE, myStyles.value);
+		}
+		public readonly void RemoveStyle(nint style)
+		{
+			myStyles.value &= ~style;
+			PInvoke.SetWindowLongPtr(NativeHandle, WINDOW_LONG_PTR_INDEX.GWL_STYLE, myStyles.value);
+		}
+		public readonly void AddOption(WINDOW_EX_STYLE option)
+		{
+			myOptions.value |= (nint) option;
+			PInvoke.SetWindowLongPtr(NativeHandle, WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE, myOptions.value);
+		}
+		public readonly void RemoveOption(WINDOW_EX_STYLE option)
+		{
+			myOptions.value &= ~(nint) option;
+			PInvoke.SetWindowLongPtr(NativeHandle, WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE, myOptions.value);
+		}
 
 		public static I As<I>()
 		{
@@ -83,7 +205,7 @@ namespace TestEditor.WinUI
 		}
 
 		[Pure]
-		public override bool Equals(object obj)
+		public override readonly bool Equals(object obj)
 		{
 			if (obj is null)
 			{
@@ -93,41 +215,41 @@ namespace TestEditor.WinUI
 			return obj.Equals(this);
 		}
 		[Pure]
-		public bool Equals(WindowView other)
+		public readonly bool Equals(WindowView other)
 		{
 			return Implement == other.Implement;
 		}
 		[Pure]
-		public override int GetHashCode()
+		public readonly override int GetHashCode()
 		{
 			return Implement.GetHashCode();
 		}
 
-		public static implicit operator Window(WindowView view)
+		public static implicit operator Window(in WindowView view)
 		{
 			return view.Implement;
 		}
-		public static implicit operator HWND(WindowView view)
+		public static implicit operator HWND(in WindowView view)
 		{
 			return view.NativeHandle;
 		}
 		[Pure]
-		public static bool operator ==(WindowView x, WindowView y)
+		public static bool operator ==(in WindowView x, in WindowView y)
 		{
 			return x.Implement == y.Implement;
 		}
 		[Pure]
-		public static bool operator !=(WindowView x, WindowView y)
+		public static bool operator !=(in WindowView x, in WindowView y)
 		{
 			return !(x == y);
 		}
 		[Pure]
-		public static bool operator ==(WindowView x, Window y)
+		public static bool operator ==(in WindowView x, in Window y)
 		{
 			return x.Implement == y;
 		}
 		[Pure]
-		public static bool operator !=(WindowView x, Window y)
+		public static bool operator !=(in WindowView x, in Window y)
 		{
 			return !(x == y);
 		}
