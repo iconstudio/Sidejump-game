a8f141b6594696a58fe4c58ed7bfdc80701c58e1
Iconer <yoyofa2@hotmail.com>
2023-09-25 16:19:35 +0900

    `ChildWindow` and `ChildWindowCollection`

---
+++ Editor/TestEditor/WinUI/ChildWindow.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics.Contracts;
+
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml.Media;
+
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
+
+namespace TestEditor.WinUI
+{
+	using WindowStyle = WINDOW_STYLE;
+	using WindowOption = WINDOW_EX_STYLE;
+
+	internal readonly struct ChildWindow : IWindowView, IDisposable
+	{
+		public readonly Window Window { get; }
+		public readonly WindowProjection Projection { get; }
+		public readonly Window Implement => ((IWindowView) Projection).Implement;
+		public readonly HWND NativeHandle => ((IWindowView) Projection).NativeHandle;
+		public readonly AppWindow AppWindow => ((IWindowView) this).AppWindow;
+		public readonly CoreWindow CoreWindow => ((IWindowView) this).CoreWindow;
+		public readonly UIElement Content => ((IWindowView) this).Content;
+		public readonly DispatcherQueue DispatcherQueue => ((IWindowView) this).DispatcherQueue;
+		public readonly bool Visible => ((IWindowView) this).Visible;
+		public readonly Rect Bounds => ((IWindowView) this).Bounds;
+		public readonly uint Dpi => ((IWindowView) this).Dpi;
+		public readonly string Title => ((IWindowView) this).Title;
+		public readonly bool ExtendsContentIntoTitleBar => ((IWindowView) this).ExtendsContentIntoTitleBar;
+		public readonly SystemBackdrop SystemBackdrop => ((IWindowView) this).SystemBackdrop;
+		public readonly WindowStyle Styles => ((IWindowView) this).Styles;
+		public readonly WindowOption Options => ((IWindowView) this).Options;
+
+		public ChildWindow(Window window, WindowProjection projection)
+		{
+			Window = window;
+			Projection = projection;
+		}
+
+		public void Dispose()
+		{
+			((IDisposable) Projection).Dispose();
+		}
+
+		[Pure]
+		public bool Equals(IWindowView other)
+		{
+			return ((IEquatable<IWindowView>) Projection).Equals(other);
+		}
+	}
+}
---
+++ Editor/TestEditor/WinUI/ChildWindowCollection.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+
+namespace TestEditor.WinUI
+{
+	internal class ChildWindowCollection
+		: IList<ChildWindow>, IReadOnlyList<ChildWindow>
+	{
+		private List<ChildWindow> myChildren = new();
+		public int Count => myChildren.Count;
+		public bool IsReadOnly => false;
+
+		public ChildWindow this[int index]
+		{
+			get => myChildren[index];
+			set => myChildren[index] = value;
+		}
+		ChildWindow IReadOnlyList<ChildWindow>.this[int index] => ((IReadOnlyList<ChildWindow>) myChildren)[index];
+
+		public Window Emplace(ChildWindow item)
+		{
+			myChildren.Add(item);
+
+			return item.Window;
+		}
+		public Window Emplace(in ChildWindow item)
+		{
+			myChildren.Add(item);
+
+			return item.Window;
+		}
+		public Window Emplace([NotNull] in Window window)
+		{
+			return Emplace(new ChildWindow(window, new(window)));
+		}
+		void ICollection<ChildWindow>.Add(ChildWindow item)
+		{
+			Emplace(item);
+		}
+		void IList<ChildWindow>.Insert(int index, ChildWindow item)
+		{
+			myChildren.Insert(index, item);
+		}
+
+		int IList<ChildWindow>.IndexOf(ChildWindow item)
+		{
+			return myChildren.IndexOf(item);
+		}
+		public void CopyTo(ChildWindow[] array, int arrayIndex)
+		{
+			myChildren.CopyTo(array, arrayIndex);
+		}
+		public IEnumerator<ChildWindow> GetEnumerator()
+		{
+			return ((IEnumerable<ChildWindow>) myChildren).GetEnumerator();
+		}
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return ((IEnumerable) myChildren).GetEnumerator();
+		}
+
+		bool ICollection<ChildWindow>.Remove(ChildWindow item)
+		{
+			return myChildren.Remove(item);
+		}
+		public void RemoveAt(int index)
+		{
+			myChildren.RemoveAt(index);
+		}
+		public void Clear()
+		{
+			myChildren.Clear();
+		}
+
+		[Pure]
+		bool ICollection<ChildWindow>.Contains(ChildWindow item)
+		{
+			return myChildren.Contains(item);
+		}
+	}
+}
