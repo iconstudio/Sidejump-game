3bb92335e4b8e7b5cf714ead75372af1e1d1181c
Iconer <yoyofa2@hotmail.com>
2023-09-30 04:41:35 +0900

    Refactoring `IWindowView` using `IWindowViewMethods`

--- Editor/TestEditor/WinUI/IWindowView.cs
+++ Editor/TestEditor/WinUI/IWindowView.cs
@@ -1,19 +1,16 @@
-﻿using System.Diagnostics.Contracts;
-
-using Microsoft.UI.Dispatching;
+﻿using Microsoft.UI.Dispatching;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml.Media;
 
 using Windows.Foundation;
 using Windows.UI.Core;
-using Windows.Win32;
 using Windows.Win32.Foundation;
 using Windows.Win32.UI.WindowsAndMessaging;
 
 namespace TestEditor.WinUI
 {
-	using WindowStyle = WINDOW_STYLE;
 	using WindowOption = WINDOW_EX_STYLE;
+	using WindowStyle = WINDOW_STYLE;
 
 	internal interface IWindowView : IEquatable<IWindowView>, IDisposable
 	{
@@ -25,7 +22,7 @@ namespace TestEditor.WinUI
 		public DispatcherQueue DispatcherQueue => Implement.DispatcherQueue;
 		public bool Visible => Implement.Visible;
 		public Rect Bounds => Implement.Bounds;
-		public uint Dpi => PInvoke.GetDpiForWindow(NativeHandle);
+		public uint Dpi => IWindowViewMethods.GetDPI(NativeHandle);
 		public string Title
 		{
 			get => Implement.Title;
@@ -36,8 +33,12 @@ namespace TestEditor.WinUI
 			get => Implement.ExtendsContentIntoTitleBar;
 			set => Implement.ExtendsContentIntoTitleBar = value;
 		}
-		public SystemBackdrop SystemBackdrop { get => Implement.SystemBackdrop; set => Implement.SystemBackdrop = value; }
-		public WindowStyle Styles => (WindowStyle) PInvoke.GetWindowLongPtr(NativeHandle, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
-		public WindowOption Options => (WindowOption) PInvoke.GetWindowLongPtr(NativeHandle, WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE);
+		public SystemBackdrop SystemBackdrop
+		{
+			get => Implement.SystemBackdrop;
+			set => Implement.SystemBackdrop = value;
+		}
+		public WindowStyle Styles => IWindowViewMethods.GetStyle(NativeHandle);
+		public WindowOption Options => IWindowViewMethods.GetExStyle(NativeHandle);
 	}
 }
