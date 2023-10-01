cd01b51836d28d588f8d8a39ef7ffc74bed69e0d
Iconer <yoyofa2@hotmail.com>
2023-10-01 20:57:30 +0900

    `Tileset`

---
+++ Editor/TestEditor/Contents/Tileset.cs
@@ -0,0 +1,22 @@
+﻿namespace TestEditor.Contents
+{
+	internal class Tileset
+	{
+		private Dictionary<int,> tileData;
+
+		public Tileset()
+		{
+		}
+		public Tileset(in SerializedTileset data)
+		{
+		}
+		public Tileset(SerializedTileset data)
+		{
+		}
+
+		public SerializedTileset Serialize()
+		{
+			return new();
+		}
+	}
+}
