9353353cd706c5954a27bedbe7dd5e24b8a0d679
Iconer <yoyofa2@hotmail.com>
2023-10-01 21:36:15 +0900

    `Tileset` can be serialized to `SerializedTileset`

--- Editor/TestEditor/Contents/Tileset.cs
+++ Editor/TestEditor/Contents/Tileset.cs
@@ -2,21 +2,42 @@
 {
 	internal class Tileset
 	{
-		private Dictionary<int,> tileData;
+		private Dictionary<int, Tile> tileData;
+		private int tileWidth;
+		private int tileHeight;
 
 		public Tileset()
 		{
+			tileData = new();
+			tileWidth = 0;
+			tileHeight = 0;
 		}
-		public Tileset(in SerializedTileset data)
+		public Tileset(in SerializedTileset data) : this()
 		{
-		}
-		public Tileset(SerializedTileset data)
-		{
+			foreach (var tile in data)
+			{
+				//tileData.Add(tile.GetID(), tile.)
+			}
+
+			tileWidth = data.tileWidth;
+			tileHeight = data.tileHeight;
 		}
 
 		public SerializedTileset Serialize()
 		{
-			return new();
+			List<SerializedTile> serialized_tiles = new();
+			serialized_tiles.EnsureCapacity(tileData.Count);
+
+			foreach (var data in tileData)
+			{
+				var tile = data.Value;
+
+				SerializedTile serialized = new(data.Key, tile.ImageFile.Path);
+
+				serialized_tiles.Add(serialized);
+			}
+
+			return new(serialized_tiles, tileWidth, tileHeight);
 		}
 	}
 }
