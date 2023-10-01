3acabb5e4ebc76f46f87ceaba2069f0e50cfd978
Iconer <yoyofa2@hotmail.com>
2023-10-01 20:57:22 +0900

    `Tile`

---
+++ Editor/TestEditor/Contents/Tile.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+
+using Windows.Storage;
+
+namespace TestEditor.Contents
+{
+	internal class Tile
+	{
+		public StorageFile ImageFile { get; init; }
+		public CanvasBitmap Source { get; private set; }
+
+
+		public SerializedTile Serialize()
+		{
+			return new();
+		}
+	}
+}
