d9ffbc71b53e8156bcf811f74e53feb2514a0767
Iconer <yoyofa2@hotmail.com>
2023-10-01 20:04:38 +0900

    `SerializedTile`

---
+++ Editor/TestEditor/Contents/SerializedTile.cs
@@ -0,0 +1,24 @@
+﻿using Windows.Storage;
+
+namespace TestEditor.Contents
+{
+	internal struct SerializedTile
+	{
+		public StorageFile tileImageFile;
+
+		public SerializedTile()
+		{
+			tileImageFile = null;
+		}
+		public SerializedTile(in StorageFile filepath)
+		{
+			tileImageFile = filepath;
+		}
+		public SerializedTile(StorageFile filepath)
+		{
+			tileImageFile = filepath;
+		}
+
+		public readonly StorageFile GetFilePath() => tileImageFile;
+	}
+}
