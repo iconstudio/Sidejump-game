19c5cc29dbee6e7778c9374cb0abd2e49d0e73aa
Iconer <yoyofa2@hotmail.com>
2023-10-01 21:15:57 +0900

    `Tile` would make a `SerializedTile`

--- Editor/TestEditor/Contents/Tile.cs
+++ Editor/TestEditor/Contents/Tile.cs
@@ -1,19 +1,22 @@
-﻿using Microsoft.Graphics.Canvas;
-using Microsoft.Graphics.Canvas.UI.Xaml;
+﻿using System.Diagnostics.Contracts;
+
+using Microsoft.Graphics.Canvas;
 
 using Windows.Storage;
 
 namespace TestEditor.Contents
 {
+	[Serializable]
 	internal class Tile
 	{
+		public int Id { get; init; }
 		public StorageFile ImageFile { get; init; }
 		public CanvasBitmap Source { get; private set; }
 
-
+		[Pure]
 		public SerializedTile Serialize()
 		{
-			return new();
+			return new(Id, ImageFile.Path);
 		}
 	}
 }
