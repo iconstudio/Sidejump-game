0959eaffedcaa371bcd358dbf6a174560c0da0f4
Iconer <yoyofa2@hotmail.com>
2023-10-02 21:51:30 +0900

    `SerializedMap`

---
+++ Editor/TestEditor/Contents/Serialized Objects/SerializedMap.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace TestEditor.Contents
+{
+	internal struct SerializedMap
+		: IEquatable<SerializedMap>, IMapSerial<Map>
+	{
+		public bool IsDeserializable => false;
+
+		public Map Deserialize()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Pure]
+		public readonly bool Equals(SerializedMap other)
+		{
+			throw new NotImplementedException();
+		}
+		[Pure]
+		public readonly override bool Equals(object obj)
+		{
+			return obj is SerializedMap other && Equals(other);
+		}
+		[Pure]
+		public readonly override int GetHashCode()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
