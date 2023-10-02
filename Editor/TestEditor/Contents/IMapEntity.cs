79d4dfa15a8a1538fd4a9b61b41f6936ff26081b
Iconer <yoyofa2@hotmail.com>
2023-10-02 19:54:59 +0900

    `IMapEntity` can perform arguments upto 7

--- Editor/TestEditor/Contents/IMapEntity.cs
+++ Editor/TestEditor/Contents/IMapEntity.cs
@@ -8,4 +8,46 @@
 	{
 		S Serialize();
 	}
+	internal interface IMapEntity<out S, in T0>
+		: IMapObject, IMapEntity
+		where S : notnull, IMapSerial
+	{
+		S Serialize(T0 arg0);
+	}
+	internal interface IMapEntity<out S, in T0, in T1>
+		: IMapObject, IMapEntity
+		where S : notnull, IMapSerial
+	{
+		S Serialize(T0 arg0, T1 arg1);
+	}
+	internal interface IMapEntity<out S, in T0, in T1, in T2>
+		: IMapObject, IMapEntity
+		where S : notnull, IMapSerial
+	{
+		S Serialize(T0 arg0, T1 arg1, T2 arg2);
+	}
+	internal interface IMapEntity<out S, in T0, in T1, in T2, in T3>
+		: IMapObject, IMapEntity
+		where S : notnull, IMapSerial
+	{
+		S Serialize(T0 arg0, T1 arg1, T2 arg2, T3 arg3);
+	}
+	internal interface IMapEntity<out S, in T0, in T1, in T2, in T3, in T4>
+		: IMapObject, IMapEntity
+		where S : notnull, IMapSerial
+	{
+		S Serialize(T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4);
+	}
+	internal interface IMapEntity<out S, in T0, in T1, in T2, in T3, in T4, in T5>
+		: IMapObject, IMapEntity
+		where S : notnull, IMapSerial
+	{
+		S Serialize(T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
+	}
+	internal interface IMapEntity<out S, in T0, in T1, in T2, in T3, in T4, in T5, in T6>
+		: IMapObject, IMapEntity
+		where S : notnull, IMapSerial
+	{
+		S Serialize(T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6);
+	}
 }
