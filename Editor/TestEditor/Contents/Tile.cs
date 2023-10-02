9324d3930171829f765c4c1b6777139f1b884da9
Iconer <yoyofa2@hotmail.com>
2023-10-02 19:47:43 +0900

    `Tile` is `IEquatable`

--- Editor/TestEditor/Contents/Tile.cs
+++ Editor/TestEditor/Contents/Tile.cs
@@ -1,17 +1,46 @@
-﻿using Microsoft.Graphics.Canvas;
+﻿using System.Diagnostics.Contracts;
+
+using Microsoft.Graphics.Canvas;
 
 namespace TestEditor.Contents
 {
 	[Serializable]
-	internal readonly struct Tile
+	internal readonly struct Tile : IEquatable<Tile>
 	{
 		public readonly string ImageFile { get; }
 		public readonly CanvasBitmap Source { get; }
 
-		internal Tile(string filepath, CanvasBitmap source)
+		public Tile(string filepath, CanvasBitmap source)
 		{
 			ImageFile = filepath;
 			Source = source;
+		}
+
+		[Pure]
+		public readonly bool Equals(Tile other)
+		{
+			return ImageFile == other.ImageFile && Source.Equals(other.Source);
+		}
+		[Pure]
+		public readonly override bool Equals(object obj)
+		{
+			return obj is Tile other && Equals(other);
+		}
+		[Pure]
+		public readonly override int GetHashCode()
+		{
+			return ImageFile.GetHashCode();
+		}
+
+		[Pure]
+		public static bool operator ==(Tile lhs, Tile rhs)
+		{
+			return lhs.Equals(rhs);
+		}
+		[Pure]
+		public static bool operator !=(Tile lhs, Tile rhs)
+		{
+			return !lhs.Equals(rhs);
 		}
 	}
 }
