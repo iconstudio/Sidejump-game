1c37adb5f9f04609c01fefb418bd1fc55e23cddd
Iconer <yoyofa2@hotmail.com>
2023-10-03 07:33:33 +0900

    `SerializedMap` would be serialized via `ToString()`

--- Editor/TestEditor/Contents/Serialized Objects/SerializedMap.cs
+++ Editor/TestEditor/Contents/Serialized Objects/SerializedMap.cs
@@ -1,17 +1,34 @@
 ﻿using System.Diagnostics.Contracts;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace TestEditor.Contents
 {
+	[Serializable]
 	internal struct SerializedMap
 		: IEquatable<SerializedMap>, IMapSerial<Map>
 	{
-		public bool IsDeserializable => false;
+		[JsonInclude] public string Tileset { get; set; }
+		[JsonInclude] public string Name { get; set; }
+		[JsonInclude] public string Description { get; set; }
+		[JsonInclude] public int X { get; set; }
+		[JsonInclude] public int Y { get; set; }
+		[JsonInclude] public int W { get; set; }
+		[JsonInclude] public int H { get; set; }
+		[JsonInclude] public int HorizontalTiles { get; set; }
+		[JsonInclude] public int VerticalTiles { get; set; }
+		[JsonIgnore] public readonly bool IsDeserializable => false;
 
 		public Map Deserialize()
 		{
 			throw new NotImplementedException();
 		}
 
+		[Pure]
+		public override readonly string ToString()
+		{
+			return JsonSerializer.Serialize(this, MapHelper.mapSaveSetting);
+		}
 		[Pure]
 		public readonly bool Equals(SerializedMap other)
 		{
