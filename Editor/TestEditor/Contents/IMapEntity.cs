231bdfc176f65744a2b36e98cf4074936c26690a
Iconer <yoyofa2@hotmail.com>
2023-10-02 19:29:28 +0900

    `IMapEntity<out S>`

--- Editor/TestEditor/Contents/IMapEntity.cs
+++ Editor/TestEditor/Contents/IMapEntity.cs
@@ -3,4 +3,9 @@
 	internal interface IMapEntity : IMapObject
 	{
 	}
+	internal interface IMapEntity<out S> : IMapObject, IMapEntity
+		where S : notnull, IMapSerial
+	{
+		S Serialize() => throw new NotImplementedException();
+	}
 }
