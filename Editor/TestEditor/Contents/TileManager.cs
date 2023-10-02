264e5f7b0cc4c11a36fe75b30684999af5264640
Iconer <yoyofa2@hotmail.com>
2023-10-02 20:17:39 +0900

    `TileManager` would load and store `Tiles`

--- Editor/TestEditor/Contents/TileManager.cs
+++ Editor/TestEditor/Contents/TileManager.cs
@@ -1,15 +1,38 @@
-﻿using Microsoft.Graphics.Canvas;
+﻿using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
+
+using Microsoft.Graphics.Canvas;
 
 namespace TestEditor.Contents
 {
-	internal class TileManager
+	internal static class TileManager
 	{
+		private static readonly ConcurrentDictionary<string, Tile> managedTiles;
 
-		public static async Task<Tile> LoadTile(string filepath)
+		static TileManager()
 		{
+			managedTiles = new();
+		}
+
+		public static async Task<Tile> LoadTile([NotNull] string filepath)
+		{
+			if (managedTiles.TryGetValue(filepath, out var existed))
+			{
+				return existed;
+			}
+
 			var bitmap = await CanvasBitmap.LoadAsync(CanvasDevice.GetSharedDevice(), filepath);
 
-			return new Tile(filepath, bitmap);
+			if (bitmap is not null)
+			{
+				var result = new Tile(filepath, bitmap);
+
+				return managedTiles.AddOrUpdate(filepath
+					, (_) => result
+					, (k, _) => throw new FileLoadException(filepath));
+			}
+
+			throw new IOException(filepath);
 		}
 		public static Task<Tile> LoadTile(SerializedTile tile)
 		{
