61f025ba732c35e8983431e8450d681d4d433e72
Iconer <yoyofa2@hotmail.com>
2023-10-03 21:47:14 +0900

    `EditorCanvas` has a Win2D canvas

--- Editor/TestEditor/Infrastructures/EditorCanvas.xaml.cs
+++ Editor/TestEditor/Infrastructures/EditorCanvas.xaml.cs
@@ -1,7 +1,13 @@
+using Microsoft.Graphics.Canvas.UI.Xaml;
+
+using Windows.Foundation;
+
 namespace TestEditor
 {
 	public sealed partial class EditorCanvas : UserControl
 	{
+		public TypedEventHandler<CanvasControl, CanvasDrawEventArgs> Draw { get; set; }
+
 		public EditorCanvas()
 		{
 			InitializeComponent();
