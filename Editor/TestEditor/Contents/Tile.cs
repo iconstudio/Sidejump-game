e871c6ccb9a4beec1799ca8c82ef4a7a8213c37d
Iconer <yoyofa2@hotmail.com>
2023-10-03 21:33:30 +0900

    `Tile` can draw to the canvas

--- Editor/TestEditor/Contents/Tile.cs
+++ Editor/TestEditor/Contents/Tile.cs
@@ -1,6 +1,9 @@
 ﻿using System.Diagnostics.Contracts;
+using System.Numerics;
 
 using Microsoft.Graphics.Canvas;
+
+using Windows.Foundation;
 
 namespace TestEditor.Contents
 {
@@ -10,7 +13,7 @@ namespace TestEditor.Contents
 	{
 		public readonly string ImageFile { get; }
 		public readonly CanvasBitmap Source { get; }
-
+		public readonly Rect Bounds => Source?.Bounds ?? Rect.Empty;
 		public bool IsDeserializable => false;
 
 		public Tile(string filepath, CanvasBitmap source)
@@ -19,6 +22,66 @@ namespace TestEditor.Contents
 			Source = source;
 		}
 
+		[Pure]
+		public readonly void Draw(CanvasDrawingSession context)
+		{
+			context.DrawImage(Source);
+		}
+		[Pure]
+		public readonly void Draw(CanvasDrawingSession context, in Vector2 position)
+		{
+			context.DrawImage(Source, position);
+		}
+		[Pure]
+		public readonly void Draw(CanvasDrawingSession context, in float x, in float y)
+		{
+			context.DrawImage(Source, x, y);
+		}
+		[Pure]
+		public readonly void Draw(CanvasDrawingSession context, in Vector2 position, in float opacity)
+		{
+			context.DrawImage(Source, position, Bounds, opacity);
+		}
+		[Pure]
+		public readonly void Draw(CanvasDrawingSession context, in float x, in float y, in float opacity)
+		{
+			context.DrawImage(Source, x, y, Bounds, opacity);
+		}
+		[Pure]
+		public readonly void DrawPart(CanvasDrawingSession context, in Rect bound)
+		{
+			context.DrawImage(Source, bound);
+		}
+		[Pure]
+		public readonly void DrawPart(CanvasDrawingSession context, in Vector2 position, in Rect bound)
+		{
+			context.DrawImage(Source, position, bound);
+		}
+		[Pure]
+		public readonly void DrawPart(CanvasDrawingSession context, in float x, in float y, in Rect bound)
+		{
+			context.DrawImage(Source, x, y, bound);
+		}
+		[Pure]
+		public readonly void DrawPart(CanvasDrawingSession context, in Vector2 position, in Rect bound, in float opacity)
+		{
+			context.DrawImage(Source, position, bound, opacity);
+		}
+		[Pure]
+		public readonly void DrawPart(CanvasDrawingSession context, in float x, in float y, in Rect bound, in float opacity)
+		{
+			context.DrawImage(Source, x, y, bound, opacity);
+		}
+		[Pure]
+		public readonly void DrawInterpolated(CanvasDrawingSession context, in Vector2 position, in float opacity, in CanvasImageInterpolation interpolation)
+		{
+			context.DrawImage(Source, position, Bounds, opacity, interpolation);
+		}
+		[Pure]
+		public readonly void DrawInterpolated(CanvasDrawingSession context, in float x, in float y, in float opacity, in CanvasImageInterpolation interpolation)
+		{
+			context.DrawImage(Source, x, y, Bounds, opacity, interpolation);
+		}
 		[Pure]
 		public readonly SerializedTile Serialize(int id)
 		{
