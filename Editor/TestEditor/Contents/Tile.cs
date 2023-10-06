0d449496e96619d1dec61b3644b0fc7cee29e2e2
Iconer <yoyofa2@hotmail.com>
2023-10-06 19:40:50 +0900

    Tile can be created from its a static factory method

--- Editor/TestEditor/Contents/Tile.cs
+++ Editor/TestEditor/Contents/Tile.cs
@@ -2,5 +2,18 @@
 {
 	internal struct Tile
 	{
+		public int id;
+		public int x;
+		public int y;
+
+		public static Tile Create(int tid, int tx, int ty)
+		{
+			return new Tile()
+			{
+				id = tid,
+				x = tx,
+				y = ty
+			};
+		}
 	}
 }
