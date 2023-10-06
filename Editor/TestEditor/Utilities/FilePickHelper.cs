5efdaac36f525398dce3b53445cfa520cb57ffc3
Iconer <yoyofa2@hotmail.com>
2023-10-07 06:41:46 +0900

    `[NotNull]` constraints

--- Editor/TestEditor/Utilities/FilePickHelper.cs
+++ Editor/TestEditor/Utilities/FilePickHelper.cs
@@ -4,6 +4,7 @@ using Windows.Storage.Pickers;
 using Windows.Win32.Foundation;
 
 using TestEditor.Editor;
+using System.Diagnostics.CodeAnalysis;
 
 namespace TestEditor.Utility
 {
@@ -11,7 +12,7 @@ namespace TestEditor.Utility
 	{
 		internal static
 			IAsyncOperation<StorageFile>
-			OpenLoadPicker(Window window, IEnumerable<string> extentions)
+			OpenLoadPicker(Window window, [NotNull] IEnumerable<string> extentions)
 		{
 			var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
 
@@ -19,7 +20,7 @@ namespace TestEditor.Utility
 		}
 		internal static
 			IAsyncOperation<StorageFile>
-			OpenLoadPicker(Window window, params string[] extentions)
+			OpenLoadPicker(Window window, [NotNull] params string[] extentions)
 		{
 			var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
 
@@ -27,7 +28,7 @@ namespace TestEditor.Utility
 		}
 		internal static
 			IAsyncOperation<StorageFile>
-			OpenLoadPicker(HWND hwnd, IEnumerable<string> extentions)
+			OpenLoadPicker(HWND hwnd, [NotNull] IEnumerable<string> extentions)
 		{
 			FileOpenPicker picker = new()
 			{
@@ -46,7 +47,7 @@ namespace TestEditor.Utility
 		}
 		internal static
 			IAsyncOperation<StorageFile>
-			OpenLoadPicker(HWND hwnd, params string[] extentions)
+			OpenLoadPicker(HWND hwnd, [NotNull] params string[] extentions)
 		{
 			FileOpenPicker picker = new()
 			{
@@ -65,7 +66,7 @@ namespace TestEditor.Utility
 		}
 		internal static
 			IAsyncOperation<StorageFile>
-			OpenSavePicker(Window window, params KeyValuePair<string, IList<string>>[] extentions)
+			OpenSavePicker(Window window, [NotNull] params KeyValuePair<string, IList<string>>[] extentions)
 		{
 			var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
 
@@ -73,7 +74,7 @@ namespace TestEditor.Utility
 		}
 		internal static
 			IAsyncOperation<StorageFile>
-			OpenSavePicker(HWND hwnd, params KeyValuePair<string, IList<string>>[] extentions)
+			OpenSavePicker(HWND hwnd, [NotNull] params KeyValuePair<string, IList<string>>[] extentions)
 		{
 			FileSavePicker picker = new();
 			picker.FileTypeChoices.Clear();
@@ -88,7 +89,7 @@ namespace TestEditor.Utility
 		}
 		internal static
 			IAsyncOperation<StorageFile>
-			OpenSavePicker(Window window, IEnumerable<KeyValuePair<string, IList<string>>> extentions)
+			OpenSavePicker(Window window, [NotNull] IEnumerable<KeyValuePair<string, IList<string>>> extentions)
 		{
 			var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
 
@@ -96,7 +97,7 @@ namespace TestEditor.Utility
 		}
 		internal static
 			IAsyncOperation<StorageFile>
-			OpenSavePicker(HWND hwnd, IEnumerable<KeyValuePair<string, IList<string>>> extentions)
+			OpenSavePicker(HWND hwnd, [NotNull] IEnumerable<KeyValuePair<string, IList<string>>> extentions)
 		{
 			FileSavePicker picker = new();
 			picker.FileTypeChoices.Clear();
