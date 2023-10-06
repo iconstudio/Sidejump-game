de6ad0b30da4f8e3e7b63aec033c0981ec26dcb2
Iconer <yoyofa2@hotmail.com>
2023-10-06 19:54:37 +0900

    `Tile` has id, x, y and ctors

--- Editor/TestEditor/Contents/Tile.cs
+++ Editor/TestEditor/Contents/Tile.cs
@@ -1,11 +1,26 @@
-﻿namespace TestEditor.Contents
+﻿using System.Diagnostics.Contracts;
+using System.Text.Json.Serialization;
+
+namespace TestEditor.Contents
 {
+	[Serializable]
 	internal struct Tile
 	{
-		public int id;
-		public int x;
-		public int y;
+		[JsonInclude] public int id;
+		[JsonInclude] public int x;
+		[JsonInclude] public int y;
 
+		public Tile() : this(0, 0, 0)
+		{ }
+		[JsonConstructor]
+		public Tile(int tid, int tx, int ty)
+		{
+			id = tid;
+			x = tx;
+			y = ty;
+		}
+
+		[Pure]
 		public static Tile Create(int tid, int tx, int ty)
 		{
 			return new Tile()
